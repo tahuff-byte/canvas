250bd3d9cb3183036601baf2e04f2c4bbd1a1bd1
tahuff-byte <70524840+tahuff-byte@users.noreply.github.com>
2021-01-13 02:23:35 -0600

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,6 +1,11 @@
 import turtle as trtl
+
+print("pick 2 colors")
+
+color_1 = input("What color would you like?: ")
+color_2 = input("can you give me another color: ")
  
-trtl.color('green','blue')
+trtl.color(color_1, color_2)
 
 trtl.begin_fill()
 for i in range(75):
