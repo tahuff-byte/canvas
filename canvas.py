5f4567558a0444c1b15fd5e2643804c95ff7a5d3
tahuff-byte <70524840+tahuff-byte@users.noreply.github.com>
2021-01-13 02:36:15 -0600

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,19 +1,25 @@
+#Imports the turtle library 
 import turtle as trtl
 
+#the turtle module pop up window
+wn = trtl.Screen()
+
+#ask to pick 2 colors
 print("pick 2 colors")
 
+#int vars so the program can gather user input
 color_1 = input("What color would you like?: ")
 color_2 = input("can you give me another color: ")
  
+#putting the color cars in the .color method paramaters
 trtl.color(color_1, color_2)
 
+#a for loop that draws the shape then shades it in based on what color you gave it
 trtl.begin_fill()
 for i in range(75):
     trtl.speed(30)
     trtl.forward(300)
     trtl.left(170)
-
 trtl.end_fill()
 
-wn = trtl.Screen()
 wn.mainloop()
